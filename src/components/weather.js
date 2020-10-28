4d8be6771095518855b2063e255a22ec39876fb1
Evgen Matysevich <evgenmatysevich@ukr.net>
2020-10-28 22:50:48 +0200

    New App Weather

---
+++ src/components/weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+class Weather extends React.Component {
+    render(){
+        return (
+            <div>
+                <h2>Погодное weather</h2>
+               
+        </div>
+    );
+    }
+}
+export default Weather;
